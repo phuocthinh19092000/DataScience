c5aaabcf17a5ef5b84b1de2656e1a759a1f72fdd
Thinh Dep Trai <phuocthinh19092000@gmail.com>
2021-05-11 01:20:42 +0700

    Analysis and Visualize

---
+++ Analysis_Visualize/testVisualiza.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+plt.style.use('classic')
+"""
+x = [1,2,3]
+y = [1,2,4]
+plt.scatter(x,y)
+plt.xlim(0,10)
+plt.ylim(0,10)
+#scatter plot color
+plt.scatter(x,y, s = 10, c ='red', marker='*')
+#add tilte
+plt.title('Test Visualize')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show() """
+
+data = np.random.multivariate_normal([0, 0], [[5, 2], [2, 2]],size=2000)
